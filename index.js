7fa48eaf9225eb7a9bd2be3b243584299ec91d8d
laps22 <lianaurora@gmail.com>
2022-12-13 18:59:50 -0500

    added directory and file structure, added inquirer package and inital consts

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
