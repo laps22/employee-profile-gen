23de6d7331c828c7adde2ecdcba53df257b0b9d8
laps22 <lianaurora@gmail.com>
2023-01-07 21:04:31 -0500

    added functions on index.js

--- index.js
+++ index.js
@@ -5,21 +5,123 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
+const generateTeam = require('./dist/team.html');
 
+const team = [];
 
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+// function for start app
+function managerQuestions() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'What is the name of the Manager?',
+        },
+        {
+            type: 'input',
+            name: 'managerID',
+            message: 'Please enter the Managers ID number.',
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'Please enter the email of the Manager.',
+        },
+        {
+            type: 'input',
+            name: 'managerOffice',
+            message: 'Please enter the office number of the Manager.',
+        },
+    ]).then(answers => {
+        const manager = new Manager(answers.managerQuestions, answers.data.push(manager));
+        menu();
 
+    });
+};
+// function for menu
+function menu() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'select',
+            message: 'Would you like to add a team member or finish building your team?',
+            choices: ['Add Engineer', 'Add Intern', 'Finished'],
+        },
+    ]).then(selected => {
+        switch (selected.employee) {
+            case 'Add Engineer':
+                engineerQuestions();
+                break;
+            case 'Add Intern':
+                internQuestions();
+                break;
+            case 'Finished':
+                generateTeam();
+                break;
+        }
+    });
+};
 
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+//function for engineer
+function engineerQuestions() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineer',
+            message: 'Please enter the Engineers name.',
+        },
+        {
+            type: 'input',
+            name: 'engineerID',
+            message: 'Please enter the Engineers ID.',
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'Please enter the Engineers email.',
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: 'Please enter the Engineers GitHub username.',
+        },
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerQuestions, answers.data.push(engineer));
+        menu();
 
+    });
+};
 
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+//funciton for intern
+function internQuestions() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'intern',
+            message: 'Please enter the Interns name.',
+        },
+        {
+            type: 'input',
+            name: 'internID',
+            message: 'Please enter the Interns ID.',
+        },
+        {
+            type: 'input',
+            name: 'internsEmail',
+            message: 'Please enter the Interns email.',
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'Please enter the Interns school name.',
+        },
 
+    ]).then(answers => {
+        const intern = new Intern(answers.internQuestions, answers.data.push(intern));
+        menu();
+    });
+};
 
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
+function createTeam() {
+    fs.writeFileSync('./dist/team.html', generateTeam(team));
+};
